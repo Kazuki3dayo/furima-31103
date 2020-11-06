f03a7c1fd6dd56725d91348e8557a88b29f2aa75
Kazuki3dayo <fate0007rx7@yahoo.co.jp>
2020-11-06 18:07:24 +0900

    単体テストコード不要コメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
-  validates :nickname,        presence: true
+  validates :nickname, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'には全角文字（漢字、ひらがな、カナ）を使用してください' } do
     validates :last_name
     validates :first_name
   end
 
-  with_options presence:true,format: {with:/\A[ァ-ン]+\z/, message: 'には全角文字（カナ）を使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'には全角文字（カナ）を使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
-  
-  validates :birth_day,       presence: true
+
+  validates :birth_day, presence: true
 end
