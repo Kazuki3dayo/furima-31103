a5003e324cda08ad64fabc80a2a344e815005c2c
Kazuki3dayo <fate0007rx7@yahoo.co.jp>
2020-11-09 14:48:33 +0900

    if文配置箇所修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -79,7 +79,7 @@ RSpec.describe Item, type: :model do
     end
 
     it 'priceの値が¥9,999,999以上では登録できないこと' do
-      @item.price = 10000000
+      @item.price = 10_000_000
       @item.valid?
       expect(@item.errors.full_messages).to include('Price は¥300〜¥9,999,999の範囲内で入力してください')
     end
@@ -87,7 +87,7 @@ RSpec.describe Item, type: :model do
     it 'priceの値は半角数字以外は登録できないこと' do
       @item.price = '５０００'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Price は¥300〜¥9,999,999の範囲内で入力してください")
+      expect(@item.errors.full_messages).to include('Price は¥300〜¥9,999,999の範囲内で入力してください')
     end
   end
 end
