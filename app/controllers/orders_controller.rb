964ff710f6c5a8b895543f81fdaafbd47439a3a5
Kazuki3dayo <fate0007rx7@yahoo.co.jp>
2020-11-14 21:01:09 +0900

    SoldOut表示追加とその他修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @order = Item.find(params[:item_id])
-    @order_shipping = OrderShipping.new
+    if current_user != @order.user && @order.order.nil?
+      @order_shipping = OrderShipping.new
+    else
+      redirect_to root_path
+    end
   end
 
   def create
     @order = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new(order_params)
-    binding.pry
     if @order_shipping.valid?
       pay_item
       @order_shipping.save
@@ -19,18 +23,17 @@ class OrdersController < ApplicationController
   end
 
   private
- def order_params
-  params.require(:order_shipping).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(order_id: params[:order_id], item_id: params[:item_id], user_id: current_user.id, token: params[:token])
- end
 
- def pay_item
-  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-  Payjp::Charge.create(
-    amount: @order[:price],  # 商品の値段
-    card: order_params[:token],    # カードトークン
-    currency: 'jpy'                 # 通貨の種類（日本円）
-  )
+  def order_params
+    params.require(:order_shipping).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(order_id: params[:order_id], item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @order[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
-
-end
-
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -4,8 +4,9 @@ class OrderShipping
 
   with_options presence: true do
     validates :post_code, :prefecture_id, :city, :house_number, :phone_number, :token
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1, message: 'の発送情報を選択してください' }
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'は正確に入力してください' }
   end
 
   def save
@@ -14,5 +15,4 @@ class OrderShipping
     # 住所の情報を保存
     Shipping.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
-
 end
